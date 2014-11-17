4e20028e7082b6327cce88d23c061cda23a6df89
Elekron <andreas.bolzyk@gmail.com>
2014-11-17 11:16:16 +0100

    bye

--- ProjektAppJava2014/src/Temp.java
+++ ProjektAppJava2014/src/Temp.java
@@ -4,6 +4,7 @@ public class Temp {
 	public static void main(String[] args) {
 		System.out.println("Git is fun");
 		System.out.println("Git is funnny or is it");
+		System.out.println("Git bye");
 
 	}
 	
